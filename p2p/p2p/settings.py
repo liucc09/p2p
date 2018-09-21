5448ca18c00758233ba6c59d7d6e394ae7c0bc86
liucc09 <liucc09@aliyun.com>
2018-09-21 17:21:17 +0800

    json done

--- p2p/p2p/settings.py
+++ p2p/p2p/settings.py
@@ -21,6 +21,8 @@ USER_AGENT = 'Mozilla/5.0'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
+FEED_EXPORT_ENCODING = 'utf-8'
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
--- p2p/p2p/spiders/p2p_spider.py
+++ p2p/p2p/spiders/p2p_spider.py
@@ -1,9 +1,13 @@
 import scrapy
 import os
+from p2p.items import P2PItem
 
 class P2PSpider(scrapy.Spider):
     item_max = 20 #设一个抓取文章的上限，防止一直抓取
-    url_max = 20 #设一个访问url的上限
+    url_max = 100 #设一个访问url的上限
+
+    item_num = 0
+    url_num = 0
     
     name = "p2p"
     allowed_domains = ["sohu.com","ifeng.com","news.baidu.com"] #将一些主要新闻网站加入抓取范围，当然baidu也得加入其中，因为翻页后还在baidu域名下面
@@ -13,23 +17,65 @@ class P2PSpider(scrapy.Spider):
 
     def parse(self, response):
         self.log(f'enter baidu : {response.url}')
-        url_res = response.css("div.result a::attr(href)").extract()
+        url_res = response.css("div.result h3.c-title a::attr(href)").extract()
+
+        url_page = response.css("p#page a::attr(href)").extract()
 
         for u in url_res:
-            self.url_max -= 1
-            if self.url_max:
-                if "news.baidu.com" in u:
-                    yield response.follow(u,callback=self.parse)
-                elif "sohu.com" in u:
-                    yield response.follow(u,callback=self.parse_sohu)
+            self.url_num += 1
+            
+            if (self.url_num < self.url_max) and (self.item_num < self.item_max):
+                self.log(f'url id {self.url_num}/{self.url_max} : {u}')
+                if "sohu.com" in u:
+                    yield scrapy.Request(u,callback=self.parse_sohu)
                 elif "ifeng.com" in u:
-                    yield response.follow(u,callback=self.parse_ifeng)
+                    yield scrapy.Request(u,callback=self.parse_ifeng)
+            else:
+                return
+
+
+        for u in url_page:
+            u = response.urljoin(u)
+            self.url_num += 1
+            
+            if (self.url_num < self.url_max) and (self.item_num < self.item_max):
+                self.log(f'url id {self.url_num}/{self.url_max} : {u}')
+                if "news.baidu.com" in u:
+                    yield scrapy.Request(u,callback=self.parse)
+            else:
+                return
 
     def parse_sohu(self,response):
-        self.log(f'parse sohu : {response.url}')
+        self.log(f'parse sohu {self.item_num}: {response.url}')
+
+        item = P2PItem()
+        item['url'] = response.url
+        item['title'] = response.css("div.text-title h1::text").extract_first()
+        item['domain'] = 'sohu.com'
+
+        item['content'] = ''.join(response.xpath("//article//text()").extract())
+        
+        self.item_num+=1
+        if self.item_num<self.item_max:
+            yield item
+            
+
 
     def parse_ifeng(self,response):
-        self.log(f'parse ifeng : {response.url}')
+        self.log(f'parse ifeng {self.item_num}: {response.url}')
+
+        
+        item = P2PItem()
+        item['url'] = response.url
+        item['title'] = response.css("h1#artical_topic::text").extract_first()
+        item['domain'] = 'ifeng.com'
+
+        item['content'] = ''.join(response.xpath("//div[@id='main_content']//text()").extract())
+        
+        self.item_num+=1
+        if self.item_num<self.item_max:
+            yield item
+            
 
         
         
